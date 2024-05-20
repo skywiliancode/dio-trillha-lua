f787600f3dc99395f7740830f1c4ba9e0fda546e
skywilian <codewilian852@gmail.com>
2024-05-20 14:53:26 -0300

    introdução a variáveis

---
+++ 2-variaveis.lua
@@ -0,0 +1,11 @@
+local name = "Wilian"
+local idade = 20
+
+print("Nome..........", name)
+print("Idade.........", idade)
+
+name = "Rafa"
+idade = 26
+print()
+print("Nome..........", name)
+print("Idade.........", idade)
