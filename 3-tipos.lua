14ed252b77b773c5f3d5485f947a62df07a11ee7
skywilian <codewilian852@gmail.com>
2024-05-20 14:54:13 -0300

    Variáveis parte 1

---
+++ 3-tipos.lua
@@ -0,0 +1,11 @@
+local write = print
+local monster = "Creeper"
+local heath = 20
+local isAngry = true
+
+write(type(monster), type(heath), type(isAngry), type(type))
+
+write()
+write("Moster........", monster)
+write("Heath........", heath)
+write("Angry........", isAngry)
